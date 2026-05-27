bf59a9e24859005cd3d95603aa7544d519124be4
poojachhabra1989 <chhabra.88@gmail.com>
2026-05-27 18:31:23 +0530

    Increase delay for password input in error message test

--- tests/VerifyErrorMessage.spec.js
+++ tests/VerifyErrorMessage.spec.js
@@ -4,7 +4,7 @@ test("verify error message", async ({page}) =>
 {
 await page.goto("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
 await page.getByPlaceholder("Username").pressSequentially("admin", {delay: 200})
- await page.locator("input[type ='password']").pressSequentially("admin1234", {delay: 200})
+ await page.locator("input[type ='password']").pressSequentially("admin1234", {delay: 300})
  await page.locator("//button[@type='submit']").click()
  await page.waitForTimeout(5000)
  const error=await page.locator("//p[@class='oxd-text oxd-text--p oxd-alert-content-text']").textContent()
